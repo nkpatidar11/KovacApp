f0e4cebf673a0440c8ca9dce0b385b296fbdc748
Nand Kishor Patidar <nk.patidar11@gmail.com>
2017-07-26 23:57:43 +0530

    26 july 17

---
+++ app/src/main/java/com/kovac/app/ActivityLifecycleAdapter.java
@@ -0,0 +1,46 @@
+package com.kovac.app;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Created by Jai Gurudev on 7/26/2017.
+ */
+
+public class ActivityLifecycleAdapter implements Application.ActivityLifecycleCallbacks {
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+}
---
+++ app/src/main/java/com/kovac/app/AppController.java
@@ -0,0 +1,31 @@
+package com.kovac.app;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+/**
+ * Created by Jai Gurudev on 7/26/2017.
+ */
+
+public class AppController extends Application {
+    public static final String TAG = AppController.class.getSimpleName();
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        registerActivityLifecycleCallbacks(new ActivityLifecycleAdapter() {
+            @Override
+            public void onActivityCreated(Activity a, Bundle savedInstanceState) {
+                a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+}
